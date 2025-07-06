43bc2c3fe963995badef264c8a3709b2a80c8fdb
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2025-07-05 21:06:31 -0300

    first commit

---
+++ src/main/java/com/outercode/Cantina/EB/CantinaEbApplication.java
@@ -0,0 +1,13 @@
+package com.outercode.Cantina.EB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CantinaEbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CantinaEbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/outercode/Cantina/EB/CantinaEbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outercode.Cantina.EB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CantinaEbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
