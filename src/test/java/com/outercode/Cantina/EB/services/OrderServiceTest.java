e78c215cee4ec876d76fa925e4e5228dd0250bc0
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2025-07-17 20:02:36 -0300

    test: test scenario find order by id

--- src/test/java/com/outercode/Cantina/EB/services/OrderServiceTest.java
+++ src/test/java/com/outercode/Cantina/EB/services/OrderServiceTest.java
@@ -2,7 +2,9 @@ package com.outercode.Cantina.EB.services;
 
 import com.outercode.Cantina.EB.dto.order.ResponseOrderDTO;
 import com.outercode.Cantina.EB.entities.Order;
+import com.outercode.Cantina.EB.entities.Product;
 import com.outercode.Cantina.EB.repositories.OrderRepository;
+import org.aspectj.weaver.ast.Or;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,11 +16,16 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.outercode.Cantina.EB.utils.InitOrderConstants.ORDER;
+import static com.outercode.Cantina.EB.utils.InitProductConstants.INVALID_CREATE_PRODUCT_DTO;
+import static com.outercode.Cantina.EB.utils.InitProductConstants.INVALID_PRODUCT;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
@@ -46,5 +53,30 @@ class OrderServiceTest {
         assertEquals(result.getFirst().items(), ORDER.getItems());
         assertEquals(result.getFirst().id(), ORDER.getId());
         assertEquals(result.getFirst().client(), ORDER.getClient());
+    }
+
+    @Test
+    void findOderById_WithValidId_ReturnOrder() {
+        when(orderRepository.findById(anyLong())).thenReturn(Optional.of(ORDER));
+
+        Order result = orderService.findById(1L);
+
+        assertNotNull(result);
+
+        assertEquals(result.getOrderStatus(),ORDER.getOrderStatus());
+        assertEquals(result.getTotalPrice(), ORDER.getTotalPrice());
+        assertEquals(result.getItems(), ORDER.getItems());
+        assertEquals(result.getId(), ORDER.getId());
+        assertEquals(result.getClient(), ORDER.getClient());
+    }
+
+    @Test
+    void findOrderById_WithInvalidId_ThenReturnObjectNotFoundException() {
+        when(orderRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        Optional<Order> sut =  orderRepository.findById(1L);
+        assertThat(sut).isEmpty();
+        verify(orderRepository).findById(1L);
+        verify(orderRepository, times(1)).findById(1L);
     }
 }
