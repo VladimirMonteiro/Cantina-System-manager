aac351edaa8993cdb9ddea1eb74e20cbbe0b8a12
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2025-07-19 13:58:14 -0300

    test: create order

--- src/test/java/com/outercode/Cantina/EB/services/OrderServiceTest.java
+++ src/test/java/com/outercode/Cantina/EB/services/OrderServiceTest.java
@@ -2,9 +2,9 @@ package com.outercode.Cantina.EB.services;
 
 import com.outercode.Cantina.EB.dto.order.ResponseOrderDTO;
 import com.outercode.Cantina.EB.entities.Order;
-import com.outercode.Cantina.EB.entities.Product;
+import com.outercode.Cantina.EB.entities.OrderItem;
+import com.outercode.Cantina.EB.repositories.OrderItemRepository;
 import com.outercode.Cantina.EB.repositories.OrderRepository;
-import org.aspectj.weaver.ast.Or;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -18,9 +18,10 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.Optional;
 
+import static com.outercode.Cantina.EB.utils.InitClientConstants.CLIENT;
+import static com.outercode.Cantina.EB.utils.InitOrderConstants.CREATE_ORDER_DTO;
 import static com.outercode.Cantina.EB.utils.InitOrderConstants.ORDER;
-import static com.outercode.Cantina.EB.utils.InitProductConstants.INVALID_CREATE_PRODUCT_DTO;
-import static com.outercode.Cantina.EB.utils.InitProductConstants.INVALID_PRODUCT;
+import static com.outercode.Cantina.EB.utils.InitProductConstants.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -36,6 +37,15 @@ class OrderServiceTest {
 
     @Mock
     private OrderRepository orderRepository;
+
+    @Mock
+    private ClientService clientService;
+
+    @Mock
+    private ProductService productService;
+
+    @Mock
+    private OrderItemRepository orderItemRepository;
 
     @Test
     void findAll_WithAllParameter_ReturnsAllOrders() {
@@ -78,5 +88,27 @@ class OrderServiceTest {
         assertThat(sut).isEmpty();
         verify(orderRepository).findById(1L);
         verify(orderRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    void createOrder_WithValidData_ReturnSuccessMessage() {
+       when(clientService.findById(anyLong())).thenReturn(CLIENT);
+       when(orderRepository.save(any())).thenReturn(ORDER);
+       when(productService.findById(anyLong())).thenReturn(PRODUCT);
+       when(orderItemRepository.save(any())).thenReturn(new OrderItem(ORDER, PRODUCT, 5.0, 1 ));
+
+
+       Order result = orderService.create(CREATE_ORDER_DTO);
+
+       assertNotNull(result);
+       assertThat(result.getClient()).isEqualTo(CLIENT);
+       assertThat(result.getOrderStatus()).isEqualTo(ORDER.getOrderStatus());
+       assertThat(result.getItems().size()).isEqualTo(CREATE_ORDER_DTO.items().size());
+       assertThat(result.getDate()).isEqualTo(ORDER.getDate());
+       assertThat(result.getTotalPrice()).isEqualTo(ORDER.getTotalPrice());
+       verify(clientService, times(1)).findById(anyLong());
+       verify(productService, times(1)).findById(anyLong());
+       verify(orderItemRepository, times(1)).save(any());
+       verify(orderRepository, times(1)).save(ORDER);
     }
 }
--- src/test/java/com/outercode/Cantina/EB/utils/InitOrderConstants.java
+++ src/test/java/com/outercode/Cantina/EB/utils/InitOrderConstants.java
@@ -1,15 +1,19 @@
 package com.outercode.Cantina.EB.utils;
 
+import com.outercode.Cantina.EB.dto.order.CreateOrderDTO;
+import com.outercode.Cantina.EB.dto.order.CreateOrderItemDTO;
 import com.outercode.Cantina.EB.entities.Order;
 import com.outercode.Cantina.EB.entities.enums.OrderStatus;
 
 import java.util.Date;
-import java.util.List;
+import java.util.Set;
 
 import static com.outercode.Cantina.EB.utils.InitClientConstants.CLIENT;
-import static com.outercode.Cantina.EB.utils.InitProductConstants.PRODUCT;
+
 
 public class InitOrderConstants {
 
     public static final Order ORDER = new Order(1L, new Date(), 4.5,OrderStatus.PAID, CLIENT);
+    public static final CreateOrderItemDTO CREATE_ORDER_ITEM_DTO = new CreateOrderItemDTO(1L, 1, 4.5);
+    public static final CreateOrderDTO CREATE_ORDER_DTO = new CreateOrderDTO(1L, new Date(), 4.5, OrderStatus.UNPAID, Set.of(CREATE_ORDER_ITEM_DTO));
 }
